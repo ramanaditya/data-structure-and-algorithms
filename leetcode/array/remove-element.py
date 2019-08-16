aa1b9852784187f56a3243d1c201f1ef303fe3a7
ramanaditya <adityaraman96@gmail.com>
2019-08-16 23:44:52 +0530

    readme

--- leetcode/array/remove-element.py
+++ leetcode/array/remove-element.py
@@ -1,7 +1,7 @@
 '''
 ## Question
 
-### 27. Remove Element
+### 27. [Remove Element](https://leetcode.com/problems/remove-element/)
 
 Given an array nums and a value val, remove all instances of that value in-place and return the new length.
 
