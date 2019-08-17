696cf8187f00955497867c2b4c58c2e582626418
ramanaditya <adityaraman96@gmail.com>
2019-08-17 16:29:28 +0530

    merge K sorted lists

--- leetcode/linked-list/remove-nth-node-from-end-of-list.py
+++ leetcode/linked-list/remove-nth-node-from-end-of-list.py
@@ -22,8 +22,6 @@ After removing the second node from the end, the linked list becomes 1->2->3->5.
 
 ## Solutions
 
-{% highlight python %}
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -54,4 +52,37 @@ class Solution:
 # Runtime: 44 ms
 # Memory Usage: 13.9 MB
 
-{% endhighlight %}
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if head == None:
+            return head
+        head_ref1 = ListNode(None)
+        head_ref1.next = head
+        head_ref = ListNode(None)
+        head_ref.next = head
+        head_ref = head_ref.next
+        count = 0
+        while head_ref != None:
+            count += 1
+            head_ref = head_ref.next
+        if n >= count:
+            return head_ref1.next.next
+        req = count - n - 1
+        while req > 0:
+            head = head.next
+            req -= 1
+        if head.next.next == None:
+            head.next = None
+        else:
+            head.next = head.next.next
+        return head_ref1.next
+
+# Runtime: 40 ms
+# Memory Usage: 13.7 MB
