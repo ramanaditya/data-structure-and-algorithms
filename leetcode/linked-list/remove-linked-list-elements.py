200821b71af516ad47fb9da7983742b4f35a005e
ramanaditya <adityaraman96@gmail.com>
2019-08-19 20:47:29 +0530

    Remove linked list elements

---
+++ leetcode/linked-list/remove-linked-list-elements.py
@@ -0,0 +1,37 @@
+'''
+203. [Remove Linked List Elements](https://leetcode.com/problems/remove-linked-list-elements/)
+
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+'''
+
+# Solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return head
+        head_ref = ListNode(None)
+        new_pointer = head_ref
+        while head:
+            if head.val != val:
+                new_node = ListNode(head.val)
+                head_ref.next = new_node
+                head_ref = head_ref.next
+            head = head.next
+        head = None
+        head_ref = None
+        return new_pointer.next
+
+# Runtime: 84 ms
+# Memory Usage: 18.5 MB
