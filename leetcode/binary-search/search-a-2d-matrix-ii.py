faecca8c34a9ab961243eefc4d02c16d22fb4f08
ramanaditya <adityaraman96@gmail.com>
2019-09-27 15:17:15 +0530

    Another program added

---
+++ leetcode/binary-search/search-a-2d-matrix-ii.py
@@ -0,0 +1,46 @@
+'''
+## Questions
+
+### 240. [Search a 2D Matrix II](https://leetcode.com/problems/search-a-2d-matrix-ii/)
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+'''
+
+## Solutions
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in matrix:
+            low = 0
+            high = len(i) - 1
+            while low <= high:
+                mid = int((low + high)/2)
+                if target == i[mid]:
+                    return True
+                elif target < i[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        return False
+
+# Runtime: 72 ms
+# Memory Usage: 15.7 MB
