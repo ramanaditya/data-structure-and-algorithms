95c6ff3b55f8dfba035b5528165b3d44bcefbf22
ramanaditya <adityaraman96@gmail.com>
2020-01-14 22:20:53 +0530

    Program updated

--- CLRS/10-Elementary-Data-Structures/10-2-2.py
+++ CLRS/10-Elementary-Data-Structures/10-2-2.py
@@ -15,45 +15,74 @@ class LinkedList:
         self.head = None
 
     def push(self, data):
+        """
+        Pushing the data in the stack at top
+        :param data: data
+        :return: None
+        """
         new_node = Node(data)
         print("Item Pushed :", end=" ")
         print(data)
         new_node.next = self.head
         self.head = new_node
+        return
 
     def pop(self):
+        """
+        Deleting the top element of the stack
+        :return: data
+        """
         curr = self.head
         if not curr:
-            print("Stack is underflow")
+            return -1
         elif not curr.next:
-            print("Item Popped :", end=" ")
-            print(curr.data)
             self.head = None
+            return curr.data
         else:
-            print("Item Popped :", end=" ")
-            print(curr.data)
             self.head = self.head.next
+            return curr.data
 
-    def printll(self):
+    def display(self):
+        """
+        Displaying the elements in the stack
+        :return: None
+        """
         curr = self.head
         if not curr:
             print("Stack is underflow")
             return
         print("Stack is : ")
+        print("top - > ", end="")
         while curr:
-            print(curr.data)
+            print(curr.data, end=" - > ")
             curr = curr.next
+        print("bottom")
 
 
-ll = LinkedList()
-ll.push(5)
-ll.push(10)
-ll.push(15)
-ll.push(20)
-ll.printll()
-ll.pop()
-ll.pop()
-ll.pop()
-ll.pop()
-ll.pop()
-ll.printll()
+if __name__ == "__main__":
+    ll = LinkedList()
+    while True:
+        print("\n")
+        print("Choice : ")
+        print("1. Push")
+        print("2. Pop")
+        print("3. Display")
+        print("4. Exit")
+        choice = int(input("Enter your choice : "))
+        if choice == 1:
+            data = input("Enter the element to be pushed : ")
+            ll.push(data)
+        elif choice == 2:
+            data = ll.pop()
+            if data != -1:
+                print("Item Popped :", end=" ")
+                print(data)
+            else:
+                print("Stack is underflow")
+        elif choice == 3:
+            ll.display()
+        elif choice == 4:
+            print("Program exiting")
+            exit()
+        else:
+            print("Invalid Choice Enter again")
