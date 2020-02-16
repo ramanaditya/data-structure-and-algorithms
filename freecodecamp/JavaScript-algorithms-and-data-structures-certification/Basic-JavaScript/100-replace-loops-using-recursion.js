addc81ad6357b55e1611914848646f5be4aa1b31
ramanaditya <adityaraman96@gmail.com>
2020-02-16 07:12:43 +0530

    Removed freecodecamp

--- freecodecamp/JavaScript-algorithms-and-data-structures-certification/Basic-JavaScript/100-replace-loops-using-recursion.js
+++
@@ -1,9 +0,0 @@
-function sum(arr, n) {
-  // Only change code below this line
-  if (n <= 0) {
-      return arr[0];
-    } else {
-      return sum(arr, n - 1) + arr[n];
-    }
-  // Only change code above this line
-}
