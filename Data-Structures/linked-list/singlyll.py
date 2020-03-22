36e0d7bc92103bccf53f96935cbc818da46cb7ab
ramanaditya <adityaraman96@gmail.com>
2020-03-22 15:52:35 +0530

    Singly Linked List operation

--- Data-Structures/linked-list/singlyll.py
+++ Data-Structures/linked-list/singlyll.py
@@ -8,11 +8,24 @@ class Node:
 
 
 class LinkedList:
+    """Operations associated with the Linked List"""
+
     def __init__(self):
         """
         Initialize the head to always point to the head of the linked list
         """
         self.head = None
+
+    def search(self, data):
+        """To search the given data in the Linked list and find it's first occurrence, if it is not present return -1"""
+        curr = self.head
+        index = 0
+        while curr:
+            if curr.data == data:
+                return index
+            index += 1
+            curr = curr.next
+        return -1
 
     def get(self, index: int):
         """
@@ -21,15 +34,15 @@ class LinkedList:
         curr = self.head
         count = 0
         if self.head is None:
-            print(-1)
+            return -1
         if index == 0:
-            print(self.head.data)
+            return self.head.data
         while curr:
             if count == index:
-                print(curr.data)
+                return curr.data
             count += 1
             curr = curr.next
-        print(-1)
+        return -1
 
     def insert_at_head(self, data):
         """Insert at head """
@@ -51,40 +64,77 @@ class LinkedList:
 
     def add_at_index(self, index: int, val: int) -> None:
         """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked
+        list, the node will be appended to the end of linked list. If index is greater than the length, the node will
+        not be inserted.
         """
+        index -= 1
         new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+            return
         curr = self.head
         if index < 1:
+            new_node.next = self.head
             self.head = new_node
+            return
         else:
             count = 0
             prev = self.head
             while curr:
                 count += 1
                 if count == index:
-                    new_node.next = prev.next
-                    prev.next = new_node
-                    break
-                prev = curr.next
+                    new_node.next = curr.next
+                    curr.next = new_node
+                    return
+                prev = curr
                 curr = curr.next
-
             if count == index:
                 curr.next = new_node
+                return
             else:
-                print(-1)
+                prev.next = new_node
+                return
+
+    def delete_head(self):
+        if not self.head:
+            return -1
+        else:
+            value = self.head.data
+            temp = self.head.next
+            self.head = None
+            self.head = temp
+            return value
+
+    def delete_tail(self):
+        if not self.head:
+            return -1
+        else:
+            curr = self.head
+            if not curr.next:
+                value = curr.data
+                self.head = None
+                return value
+            while curr.next.next:
+                curr = curr.next
+            value = curr.next.data
+            curr.next = None
+            return value
 
     def delete_at_index(self, index: int):
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
+        index -= 1
         if self.head is None:
-            print(-1)
+            return -1
         curr = self.head
         if index == 0:
+            value = curr.data
             self.head = curr.next
+            return value
         elif index < 0:
-            print(-1)
+            return -1
         else:
             for i in range(index - 1):
                 curr = curr.next
@@ -94,10 +144,11 @@ class LinkedList:
                 return -1
             if curr.next is None:
                 return -1
-
+        value = curr.data
         next = curr.next.next
         curr.next = None
         curr.next = next
+        return value
 
     def printll(self):
         curr = self.head
@@ -107,24 +158,79 @@ class LinkedList:
         print("/")
 
 
-ll = LinkedList()
-ll.insert_at_tail(40)
-ll.insert_at_head(5)
-ll.insert_at_head(10)
-ll.get(1)
-ll.printll()
-ll.get(3)
-ll.insert_at_head(15)
-ll.insert_at_tail(20)
-ll.printll()
-ll.get(3)
-ll.add_at_index(3)
-ll.get(3)
-ll.delete_at_index(3)
-ll.printll()
+if __name__ == "__main__":
+    ll = LinkedList()
+    while True:
+        print("\n======================")
+        print("Linked List Operations")
+        print("======================")
+        print("Select the operation")
+        print("1. Get the Node")
+        print("2. Search")
+        print("3. Insert at Head")
+        print("4. Insert at Tail")
+        print("5. Insert at Index")
+        print("6. Delete at head")
+        print("7. Delete at Tail")
+        print("8. Delete at Index")
+        print("9. Display Linked List")
+        print("10. Exit")
+        choice = int(input("Choice : "))
+        print("\n---------------")
+        if choice == 1:
+            index = int(input("Enter the index to get the value of the node : "))
+            node = ll.get(index)
+            if node == -1:
+                print("Node Does not exist at index {}".format(index))
+            else:
+                print("node '{}' is present at index : {}".format(node, index))
+        elif choice == 2:
+            val = int(input("Enter the value to be searched : "))
+            index = ll.search(val)
+            if index == -1:
+                print("node '{}' is not present".format(val))
+            else:
+                print("node '{}' is present at index : {}".format(val, index))
 
-
-"""
-Insertion at the head is only done in O(1) time
-DELETE operation can not be performed in O(1) time
-"""
+        elif choice == 3:
+            node = int(input("Enter the val of node to be inserted : "))
+            ll.insert_at_head(node)
+            ll.printll()
+        elif choice == 4:
+            node = int(input("Enter the val of node to be inserted : "))
+            ll.insert_at_tail(node)
+            ll.printll()
+        elif choice == 5:
+            index = int(input("Enter the index at which node will be inserted : "))
+            node = int(input("Enter the val of node to be inserted : "))
+            ll.add_at_index(index, node)
+            ll.printll()
+        elif choice == 6:
+            value = ll.delete_head()
+            if value == -1:
+                print("Linked List does not exist")
+            else:
+                print("node '{}' has been deleted from head".format(value))
+            ll.printll()
+        elif choice == 7:
+            value = ll.delete_tail()
+            if value == -1:
+                print("Linked List does not exist")
+            else:
+                print("node '{}' has been deleted from tail".format(value))
+            ll.printll()
+        elif choice == 8:
+            index = int(input("Enter the index to delete the node : "))
+            value = ll.delete_at_index(index)
+            if value == -1:
+                print("Node does not exist")
+            else:
+                print("node '{}' has been deleted at index {}".format(value, index))
+            ll.printll()
+        elif choice == 9:
+            ll.printll()
+        elif choice == 10:
+            quit()
+        else:
+            print("Invalid Choice")
+        print("---------------")
