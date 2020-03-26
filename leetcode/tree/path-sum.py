425b32b7433cbbd547cd462ea8ce79cebcfc38aa
ramanaditya <adityaraman96@gmail.com>
2020-03-26 16:57:22 +0530

    Path sum
    leetcode
    python

---
+++ leetcode/tree/path-sum.py
@@ -0,0 +1,46 @@
+"""
+[112. Path Sum](https://leetcode.com/problems/path-sum/)
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along
+the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.total_sum = 0
+
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right and sum - root.val == 0:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(
+            root.right, sum - root.val
+        )
+
+
+# Runtime: 44 ms, faster than 52.07% of Python3 online submissions
+# Memory Usage: 14.4 MB, less than 100.00% of Python3 online submissions
